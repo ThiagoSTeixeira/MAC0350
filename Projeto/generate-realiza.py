6d31a9342b2dfb02e257a3eb9c2d3cad14ae80e1
Édio Cerati Neto <edioceratineto@gmail.com>
2020-05-31 19:54:01 -0300

    Update random generator

--- Projeto/generate-realiza.py
+++ Projeto/generate-realiza.py
@@ -1,16 +1,29 @@
 import random as rd
 
-def timestamp():
-    month = str(rd.randint(3, 5))
-    day = str(rd.randint(10, 30))
-    hour = str(rd.randint(10, 17))
-    minute = str(rd.randint(10,60))
-    seconds = str(rd.randint(10, 60))
+def horario():
+    hour = str(rd.randint(8, 17)).zfill(2)
+    minute = str(rd.randint(1,60)).zfill(2)
+    seconds = str(rd.randint(1, 60)).zfill(2)
 
-    return f'2020-0{month}-{day} {hour}:{minute}:{seconds}'
+    return f'{hour}:{minute}:{seconds}'
+
+def timestamp():
+    month = str(rd.randint(1, 5)).zfill(2)
+    day = str(rd.randint(1, 28)).zfill(2)
+
+    executionDay = int(day) + rd.randint(1, 7)
+    executionMonth = int(month)
+    if executionDay > 28:
+        executionMonth = executionMonth + 1
+        executionDay = executionDay - 28
+
+    executionMonth = str(executionMonth).zfill(2)
+    executionDay = str(executionDay).zfill(2)
+
+    return f'2020-{month}-{day} {horario()}', f'2020-{executionMonth}-{executionDay} {horario()}'
 
 insert = 'INSERT INTO public.realiza'
-tuples = '    (id_paciente, id_exame, codigo_amostra, data_de_realizacao)'
+tuples = '    (id_paciente, id_exame, codigo_amostra, data_de_solicitacao, data_de_realizacao)'
 
 id_paciente = []
 for i in range(1, 11):
@@ -31,4 +44,5 @@ for i in range(15):
 for i in range(15):
     print(insert)
     print(tuples)
-    print(f'VALUES({id_paciente[i]}, {id_exame[i]}, \'{codigo_amostra[i]}\', \'{timestamp()}\');\n')
+    data1, data2 = timestamp()
+    print(f'VALUES({id_paciente[i]}, {id_exame[i]}, \'{codigo_amostra[i]}\', \'{data1}\', \'{data2}\');\n')
